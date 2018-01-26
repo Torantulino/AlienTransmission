8bb229bb92c4bcc0b785901432c28d846fec9cf9
John Turner <git@norgg.org>
2018-01-26 21:47:11 +0000

    Have soldiers report interesting things.

---
+++ Assets/Scripts/Interesting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interesting : MonoBehaviour {
+	// Marks things as being interesting enough for soldiers to report.
+	public string name;
+
+	void Start() {
+	}
+	
+}
--- Assets/Scripts/SoldierReport.cs
+++ Assets/Scripts/SoldierReport.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoldierReport : MonoBehaviour {
-	int numRays = 20;
+	public int numRays = 20;
 	public float fieldOfView = 45;
 	public float viewDistance = 5;
 
@@ -15,16 +15,22 @@ public class SoldierReport : MonoBehaviour {
 			float offset = (i - (numRays / 2)) / (float)numRays;
 			float angle = offset * fieldOfView * 2;
 			Vector3 rayDir = Quaternion.AngleAxis(angle, Vector3.up) * transform.forward;
-			Debug.Log(rayDir);
 
 			Ray ray = new Ray(transform.position, rayDir);
 			RaycastHit hit;
 
 			if (Physics.Raycast(ray, out hit, viewDistance)) {
 				Debug.DrawLine(transform.position, transform.position + rayDir * viewDistance, Color.red);
+				if (hit.collider.GetComponent<Interesting>()) {
+					seen.Add(hit.collider.gameObject);
+				}
 			} else {
 				Debug.DrawLine(transform.position, transform.position + rayDir * viewDistance);
 			}
+		}
+
+		foreach(var thing in seen) {
+			Debug.Log(thing.GetComponent<Interesting>().name);
 		}
 	}
 }
