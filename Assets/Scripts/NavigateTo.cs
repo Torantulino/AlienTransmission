77ca5445f5c0714ea71259064c13f4eff3cb0f09
John Turner <git@norgg.org>
2018-01-26 20:07:10 +0000

    Initial commit, basic pathfinding.

---
+++ Assets/Scripts/NavigateTo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigateTo : MonoBehaviour {
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				GetComponent<NavMeshAgent>().destination = hit.point;
+			}
+		}
+	}
+}
