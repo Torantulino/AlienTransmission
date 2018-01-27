6aea7fc4c30da17c95411af6920a245a53c250f7
ZayadNimrod <32575659+ZayadNimrod@users.noreply.github.com>
2018-01-27 13:50:11 +0000

    BSP dungeon generation
    currently, only a dungeon is generated, but no monster or player placement

---
+++ Assets/Scripts/ProcGenHelpers/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.ProcGenHelpers
+{
+    class Rectangle
+    {
+        public int x;
+        public int y;
+
+        public int width;
+        public int height;
+
+        public Rectangle(int x, int y,int w, int h) {
+            this.x = x;
+            this.y = y;
+            this.width = w;
+            this.height = h;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/ProcGenHelpers/Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.ProcGenHelpers
+{
+    class Tree<T>
+    {
+        public T value;
+        public Tree<T> left;
+        public Tree<T> right;
+
+        public Tree(T val, Tree<T> l = null, Tree<T> r = null)
+        {
+            value = val;
+            left = l;
+            right = r;
+        }
+
+
+    }
+}
