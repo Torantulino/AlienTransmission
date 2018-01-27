f970535467661e6c3b5626eed55f623b2302dc83
Torantulino <40276179@live.napier.ac.uk>
2018-01-27 05:19:10 +0000

    UI Redesign, Camera PP, TextInput.

---
+++ Assets/UI/UCForcer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UCForcer : MonoBehaviour
+{
+
+    public InputField inputField;
+
+    public void Start()
+    {
+       //Add Listener to call ValChanged
+        inputField.onValueChange.AddListener(delegate {ValChanged(); });
+    }
+
+    public void ValChanged()
+    {
+        inputField.text = inputField.text.ToUpper();
+        Debug.Log("test");
+    }
+}
