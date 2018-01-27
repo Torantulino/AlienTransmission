d444cfabeb9c7ce5eb21680ca9d7fcd9843eefb7
John Turner <git@norgg.org>
2018-01-27 12:27:59 +0000

    Health and alien attack.

--- Assets/Scripts/AlienAI.cs
+++ Assets/Scripts/AlienAI.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class AlienAI : MonoBehaviour {
 	public bool canAct;
 	public float detectionRange = 10;
+	public float attackRange = 2;
 
 	NavMeshAgent agent;
 	Transform target;
@@ -19,15 +20,22 @@ public class AlienAI : MonoBehaviour {
 		if (canAct) {
 			if (target) {
 				agent.destination = target.position;
+
+				if (Vector3.Distance(target.position, transform.position) < attackRange) {
+					target.GetComponent<Health>().Damage();
+				}
+
 				if (Vector3.Distance(target.position, transform.position) > detectionRange) {
 					// Target is too far away, lose track of them but continue going to previous position.
+					Debug.Log("Lost target");
 					target = null;
 				}
+
 			} else {
 				// No target, find a new one
 				foreach (var collider in Physics.OverlapSphere(transform.position, detectionRange)) {
 					var soldier = collider.GetComponent<SoldierReport>();
-					if (soldier) {
+					if (soldier && Vector3.Distance(soldier.transform.position, transform.position) < detectionRange) {
 						target = soldier.transform;
 						break;
 					}
