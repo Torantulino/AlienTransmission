197a00acbde86043ec22789a8b4bd64cf24f7e9f
John Turner <git@norgg.org>
2018-01-27 15:23:17 +0000

    Add tutorial level into mainvew and navmesh, move stuff back to the origin.

---
+++ Assets/Scripts/GridCoords.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCoords {
+	public Camera AOCam;
+	public Vector2Int gridPartitions = new Vector2Int(20, 20);
+
+	/*public Vector3 GridCoordToWorld(string gridCoord) {
+		int x = gridCoord.ToUpper().ToCharArray()[0] - 'A';
+		int y = int.Parse(gridCoord.Substring(1)) - 1;
+
+		return new Vector3(x * gridSize, y * gridSize);
+	}*/
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+}
