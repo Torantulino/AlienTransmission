eeaa023b9f9cc75c27d28d6e1ed2c2fefead0429
Dan Allison <danieliallison@gmail.com>
2018-01-27 13:20:40 +0000

    Command updates - still test/hard coded stuff

---
+++ Assets/Scripts/DirectionEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    public enum DirectionEnum
+    {
+        North = 1,
+        NorthEast = 2,
+        East = 3,
+        SouthEast = 4,
+        South = 5,
+        SouthWest = 6,
+        West = 7,
+        NorthWest = 8
+    }
+
