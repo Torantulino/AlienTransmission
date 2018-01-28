addbcf64caed89ce12ea508589ef6c367d24a011
ZayadNimrod <32575659+ZayadNimrod@users.noreply.github.com>
2018-01-28 10:47:43 +0000

    "linedef" procedural generation algorithm
    This is awful, generates unreachable areas, and has no way of knowing right now, so alien spawning is disabled for now.

---
+++ Assets/Scripts/ProcGenHelpers/Linedef.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.ProcGenHelpers
+{
+    class Linedef
+    {
+        public Point start;
+        public Point end;
+
+ 
+        public Linedef(Point a, Point b) {
+            start = a;
+            end = b;
+        }
+
+
+
+        ///true: x, false:y
+        public bool Direction() {
+            if (start.x == end.x) { return false; }
+            if (start.y == end.y) { return true; }
+            throw new Exception();
+        }
+
+        public Point AveragePoint() {
+            return new Point((start.x+end.x)/2, (start.y + end.y) / 2);
+        }
+
+        public float Length() {            
+            return (float)Math.Sqrt(((start.x-end.x)* (start.x - end.x)) + ((start.y - end.y) * (start.y - end.y)));
+        }
+    }
+}
