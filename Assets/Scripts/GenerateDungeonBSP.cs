ab01265c26950acec3d82eae65a772dbc55f1f7b
ZayadNimrod <32575659+ZayadNimrod@users.noreply.github.com>
2018-01-28 12:08:13 +0000

    players are now spawned and set up properly

--- Assets/Scripts/GenerateDungeonBSP.cs
+++ Assets/Scripts/GenerateDungeonBSP.cs
@@ -125,6 +125,27 @@ public class GenerateDungeonBSP : MonoBehaviour
                 d.transform.position = new Vector3(x - dimension / 2, 0, y + 7 - dimension / 2);
                 playerPositions.Add(new Point(x, y+7));
 
+                //set up command controller
+
+                GameObject turnManagerObj = GameObject.Find("Turn Manager");
+                CommandController cc = turnManagerObj.GetComponent<CommandController>();
+                cc.SoldierList.Add(a.GetComponent<SoldierInfo>());
+                cc.SoldierList.Add(b.GetComponent<SoldierInfo>());
+                cc.SoldierList.Add(c.GetComponent<SoldierInfo>());
+                cc.SoldierList.Add(d.GetComponent<SoldierInfo>());
+
+                TurnManager turnManagerComponent = turnManagerObj.GetComponent<TurnManager>();
+                turnManagerComponent.SoldierList[0]=(a.GetComponent<SoldierCommands>());
+                turnManagerComponent.SoldierList[1]=(b.GetComponent<SoldierCommands>());
+                turnManagerComponent.SoldierList[2]=(c.GetComponent<SoldierCommands>());
+                turnManagerComponent.SoldierList[3]=(d.GetComponent<SoldierCommands>());
+
+                UIIOMan uIIOMan = GameObject.Find("UIIOManager").GetComponent<UIIOMan>();
+                uIIOMan.alphasp = a.transform.GetChild(0).gameObject;
+                uIIOMan.bravosp = b.transform.GetChild(0).gameObject;
+                uIIOMan.charliesp = c.transform.GetChild(0).gameObject;
+                uIIOMan.deltasp = d.transform.GetChild(0).gameObject;
+
                 break;
             }
 
@@ -138,6 +159,10 @@ public class GenerateDungeonBSP : MonoBehaviour
 
         List<Point> alienPositions = new List<Point>();
         int tries = Random.Range(minAlienTries, maxAlienTries);
+
+        GameObject turnManager = GameObject.Find("Turn Manager");
+        TurnManager tm = turnManager.GetComponent<TurnManager>();
+        List<AlienAI> ais = new List<AlienAI>();
         for (int i = tries; i > 0; i--)
         {
             //try to place an ayy
@@ -146,13 +171,18 @@ public class GenerateDungeonBSP : MonoBehaviour
 
             if (tiles[x, y] == false)
             {
+                Debug.Log("checking for player proximity");
                 //ayy lamoas dont start too close to player start position
                 if (playerPositions.Find(p => Mathf.Sqrt(((p.x - x) ^ 2) + ((p.y - y) ^ 2)) < maxAlienSpawnPlayerSpacing) != null)
                 {
-
+                    Debug.Log("check for friend proximity");
                     if (alienPositions.Find(p => Mathf.Sqrt(((p.x - x) ^ 2) + ((p.y - y) ^ 2)) < maxAlienSpawnSpacing) != null)
                     { //make sure ayy lmaos srent spawned to close to each other
+
+
                         GameObject p = GameObject.Instantiate(alien);
+
+                        ais.Add(p.GetComponent<AlienAI>());
                         alienPositions.Add(new Point(x, y));
                         p.transform.position = new Vector3(x - dimension / 2, 2, y - dimension / 2);
                     }
@@ -161,7 +191,10 @@ public class GenerateDungeonBSP : MonoBehaviour
 
         }
 
-
+        tm.AlienList = new AlienAI[ais.Count];
+        for (int  i = 0; i<ais.Count; i++) {
+            tm.AlienList[i] = ais[i];
+        }
 
 
     }
